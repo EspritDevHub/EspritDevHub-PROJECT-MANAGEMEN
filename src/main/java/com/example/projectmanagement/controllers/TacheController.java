47cc7513db0d0efedf830f5dc0b0f28838731133
seyf <seifeddine.hammami@esprit.tn>
2025-04-27 16:36:30 +0100

    second push

---
+++ src/main/java/com/example/projectmanagement/controllers/TacheController.java
@@ -0,0 +1,4 @@
+package com.example.projectmanagement.controllers;
+
+public class TacheController {
+}
---
+++ src/main/java/com/example/projectmanagement/iservices/IProjet.java
@@ -0,0 +1,4 @@
+package com.example.projectmanagement.iservices;
+
+public interface IProjet {
+}
