0938d97659146ca2f79e1d0aa06a371a4dc14a2d
seyf <seifeddine.hammami@esprit.tn>
2025-04-27 19:14:30 +0100

    projection

---
+++ src/main/java/com/example/projectmanagement/Projection/TacheProjection.java
@@ -0,0 +1,17 @@
+package com.example.projectmanagement.Projection;
+
+
+public interface TacheProjection {
+
+
+    
+    public Long getId();
+    public String grtTitre();
+    public String getDescription();
+    public String getStatut();
+    public String getDateDebut();
+    public String getDateFin();
+        
+
+    
+}
